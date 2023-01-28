0007f3891d6db7978bcb5e60264bb03e1faff05a
Александр Киселев <111588369+AleksandrKis@users.noreply.github.com>
2023-01-28 20:32:44 +0300

    Update first.java
    change name class to MyFirstClass

--- first.java
+++ first.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class MyClass {
+class MyFirstClass {
 
     public static void main(String[] args) {
         List<String> name = new ArrayList<>();
